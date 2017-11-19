abc332d887464da2ff361cccf20300faeb911e5d
WiseNN <wise.norris1@gmail.com>
2017-11-18 19:20:01 -0500

    modifying web process type in pom.xml to fix heroku deployment failure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.*;
+
 import static spark.Spark.*;
 
 
@@ -6,16 +9,54 @@ public class Main
 
     public static void main(String[] args)
     {
+        //do not touch
         port(getHerokuAssignedPort());
 
-        get("/hello", (req, res) -> "Hello Heroku World");
+        Main obj1 = new Main();
+
+        get("/encodeData", (req, res) -> obj1.encryptionFunction());
         get("/", (req,res) -> "Hello There Heroku for Java, nice for you to have joined us! ;)");
     }
 
 
+
+
+    String encryptionFunction()
+    {
+
+        String myStr = "Hello World";
+
+        for(int i=0;i<myStr.length();i++)
+        {
+            int randomNum = (int)Math.random();
+
+            int intForChar = myStr.charAt(0);
+
+            char newLetter = (char)(intForChar + randomNum);
+
+        }
+
+
+        return "Some New Data";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
+
         if (processBuilder.environment().get("PORT") != null) {
+
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
 
