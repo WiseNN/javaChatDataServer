7386b1e96874e447a14d7a2efb7a1a70c0491b25
WiseNN <wise.norris1@gmail.com>
2017-11-22 12:15:34 -0500

    added org.JSON library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,57 +2,41 @@ import java.util.HashMap;
 import java.util.*;
 
 import static spark.Spark.*;
+import dataProcessing.Encryption;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+
 
 
 public class Main
 {
-
+    static int num = 0;
     public static void main(String[] args)
     {
         //do not touch
         port(getHerokuAssignedPort());
+        JSONObject obj = new JSONObject();
 
-        Main obj1 = new Main();
+        Encryption op1 = new Encryption();
 
-        get("/encodeData", (req, res) -> obj1.encryptionFunction());
+
+
+        //I am going to send a String, HashMap<String, String>
+        get("/encodeData/:data", (req, res) -> req.params("data"));
+        get("/decodeData", (req, res) -> "Decoding Data");
         get("/", (req,res) -> "Hello There Heroku for Java, nice for you to have joined us! ;)");
     }
 
 
 
 
-    String encryptionFunction()
+
+
+
+    static int getHerokuAssignedPort()
     {
-
-        String myStr = "Hello World";
-
-        for(int i=0;i<myStr.length();i++)
-        {
-            int randomNum = (int)Math.random();
-
-            int intForChar = myStr.charAt(0);
-
-            char newLetter = (char)(intForChar + randomNum);
-
-        }
-
-
-        return "Some New Data";
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
 
         if (processBuilder.environment().get("PORT") != null) {
